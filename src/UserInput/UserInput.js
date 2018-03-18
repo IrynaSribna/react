37c6f38bb741960337abcd395415772cc9f73ffc
IrynaSribna <iryna.sribna@zanox.com>
2018-03-18 22:00:26 +0100

    Add two components and styles

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './UserInput.css'
+
+const userInput = (props) => {
+    return (
+        <input className="UserInput" type="text" onChange={props.changed} value={props.name}/>
+    )
+
+}
+
+export default userInput
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './UserOutput.css'
+
+const userOutput = (props) => {
+    return (
+        <div className="UserOutput">   
+            <p>User name: <span>{props.name}</span></p>
+            <p>Hello <span>{props.name}!</span></p>
+    </div>
+    )
+}
+
+export default userOutput
